130dffbd2dc8498574f4ba0bf88513eb65790d94
petabolaji0 <148864188+petabolaji0@users.noreply.github.com>
2023-10-24 23:07:05 +0100

    Add files via upload

---
+++ ADS 1 - Visualisation and repository.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 11:25:36 2023
+@author: pc
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# QUESTION 2(a)
+# import csv files
+BCS = pd.read_csv("BCS_ann.csv")
+print('\nBarclays:\n', BCS)
+
+BP = pd.read_csv("BP_ann.csv")
+print('\nBP:\n', BP)
+
+TSCO = pd.read_csv("TSCO_ann.csv")
+print('\nTESCO:\n', TSCO)
+
+VOD = pd.read_csv("VOD_ann.csv")
+print('\nVodafone:\n', VOD)
+
+# plot histogram
+plt.figure()
+plt.subplot(2, 2, 1)  # subplot count starts at 1
+plt.hist(BCS['ann_return'], label="Barclays", density=True, color='cyan')
+plt.legend()
+
+plt.subplot(2, 2, 2)  # subplot count starts at 2
+plt.hist(BP['ann_return'], label="BP", density=True, color='blue')
+plt.legend()
+
+plt.subplot(2, 2, 3)  # subplot count starts at 3
+plt.hist(TSCO['ann_return'], label="TESCO", density=True, color='red')
+plt.legend()
+
+plt.subplot(2, 2, 4)  # subplot count starts at 4
+plt.hist(VOD['ann_return'], label ="Vodafone", density=True, color='magenta')
+plt.legend()
+
+plt.show()
+
+# QUESTION 2(b)
+# take two stocks to create combined histogram
+plt.figure()
+plt.hist(BP['ann_return'], label="BP", density=True, alpha=0.7)
+plt.hist(TSCO['ann_return'], label="TESCO", density=True, alpha=0.6)
+plt.legend()
+plt.show()
+
+# QUESTION 2(c)
+# create a boxplot for return distribution of the four companies
+plt.figure()
+plt.boxplot([BCS['ann_return'], BP['ann_return'], TSCO['ann_return'], \
+             VOD['ann_return']], labels=["Barclays", "BP", "TESCO", "Vodafone"])
+plt.show()
+
+# save figure as .png
+plt.savefig('Visualisation.png')
+
+
+# QUESTION 3(a)
+market_cap = np.array([33367, 68785, 20979, 29741])
+company = ["Barclays", "BP", "TESCO", "Vodafone"]
+
+# plot pie chart for the 4 companies
+plt.figure()
+plt.pie(market_cap, labels=company)
+plt.title("Market Capitalisation (£)")
+plt.show()
+
+# Question 3(b)
+# Market capitalisation as a fraction of total market capitalisation
+total_market_cap = 1814000
+new_market_cap = market_cap / total_market_cap
+
+# plot pie chart
+plt.figure()
+plt.pie(new_market_cap, labels=company)
+plt.title("New Market Capitalisation (£)")
+plt.show()
+
+# Question 3(c)
+# create a bar plot
+plt.figure()
+plt.bar(company, market_cap)
+plt.title("Market Capitalisation (£)")
+plt.ylabel("Market Cap")
+plt.xlabel("Company")
+plt.show()
---
+++ ADS 1- Pandas and Pep 8 Exercise.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 11:45:45 2023
+@author: pc
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# QUESTION 1
+# read csv file
+countries  = pd.read_csv("countries_top10.csv")
+print('\n', countries)
+
+# convert into dataframe
+countries_top10 = pd.DataFrame(data=countries, \
+                        columns=("Country", "Population",  "Area", "GDP"))
+print('\n', countries_top10)
+
+# create new column Population/Area
+countries_top10["Population/Area"] = countries_top10["Population"] / \
+    countries_top10["Area"]
+print('\n', countries_top10)
+
+# create new column of GDP per head
+countries_top10["GDP/Population"] = countries_top10["GDP"] / countries["Population"]
+print('\n', countries_top10)
+
+# writing into an excel file
+countries_top10.to_excel("countries_top10.xlsx")
+
+# QUESTION 2
+# read the xlsx file
+GDP_2015dollars = pd.read_excel("GDP_2015dollars.xlsx")
+print('\n', GDP_2015dollars)
+
+# convert into dataframe
+GDP_2015dollars = pd.DataFrame(data=GDP_2015dollars, \
+            columns=("Year", "China",  "Germany", "Japan", "United States"))
+print('\n', GDP_2015dollars)
+
+# plot all four times series
+plt.figure()
+plt.plot(GDP_2015dollars["Year"], GDP_2015dollars["China"], ":", label="China")
+plt.plot(GDP_2015dollars["Year"], GDP_2015dollars["Germany"], "--", label="Germany")
+plt.plot(GDP_2015dollars["Year"], GDP_2015dollars["Japan"], "-.", label="Japan")
+plt.plot(GDP_2015dollars["Year"], GDP_2015dollars["United States"], "-", label="United States")
+
+plt.title('Countries/Year')
+plt.xlabel("Year")
+plt.ylabel("Countries")
+plt.legend()
+
+plt.show()
+
+# extract and print data from 2011 to 2020
+GDP_2015dollars_2011_2020 = (GDP_2015dollars[41:])
+print('\n GDP_2015dollars_2011_2020:\n', GDP_2015dollars_2011_2020)
+
